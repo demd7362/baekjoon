00598a210bf73d43af36c8646b3d19e95ce752b5
Jihun Jeong <demd7362@gmail.com>
2023-09-02 13:24:05 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.18 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.js
+++ 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.js
@@ -1,9 +1,12 @@
 function solution(new_id) {
-    new_id = new_id.toLowerCase().replace(/[^a-z0-9-_.]/g, '').replace(/\.{2,}/g, '.').replace(/^\.|(\.$)/g, '') || 'a';
+    new_id = (new_id.toLowerCase()
+        .replace(/[^a-z0-9-_.]/g, '')
+        .replace(/\.{2,}/g, '.')
+        .replace(/^\.|(\.$)/g, '') || 'a')
+        .substring(0, 15)
+        .replace(/\.$/,'');
 
-    if (new_id.length >= 16) {
-        new_id = new_id.substring(0, 15).replace(/\.$/,'');
-    }
+    
     const lastWord = new_id[new_id.length-1];
     while (new_id.length < 3) {
         new_id += lastWord;
