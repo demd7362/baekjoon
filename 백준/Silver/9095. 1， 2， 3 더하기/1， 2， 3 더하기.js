8c8068a756519bb2589850eb4df1bd777f3a76c2
Jihun Jeong <demd7362@gmail.com>
2023-12-03 11:43:16 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 124 ms, Memory: 9368 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.js
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.js
@@ -10,12 +10,21 @@ let input = fs.readFileSync(filePath)
 
 function solution([count,...args]) {
 
-    for(const arg of args){
-        const dp = [1,1,2];
-        for(let i = 3; i <= arg; i++){
+    for (const arg of args) {
+        if (arg < 2) {
+            console.log(1);
+            continue; // 다음 arg로 넘어갑니다.
+        }
+        if (arg === 2) {
+            console.log(2);
+            continue; // 다음 arg로 넘어갑니다.
+        }
+
+        const dp = [1, 1, 2]; // n=0, n=1, n=2일 때의 초기값 설정
+        for (let i = 3; i <= arg; i++) {
             dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
         }
-        console.log(dp[arg]);
+        console.log(dp[arg]); // arg에 해당하는 값을 출력
     }
 }
 
