3c0fd53ff8302a2add14b620690758815adaac89
Jihun Jeong <demd7362@gmail.com>
2023-12-15 18:34:26 +0900

    [Bronze II] Title: 단어의 개수, Time: 224 ms, Memory: 22196 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath)
+    .toString()
+    .trim()
+// .split("\n")
+// .map(x => x.trim());
+
+
+function solution(input) {
+    const answer = input.split(' ').filter(x => x !== '').length;
+    console.log(answer)
+}
+
+
+solution(input);
