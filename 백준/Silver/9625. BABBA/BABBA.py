c998046aa7fd625cecb2c25c0613a667c5cc4ba8
Jihun Jeong <demd7362@gmail.com>
2023-12-24 13:01:37 +0900

    [Silver V] Title: BABBA, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/9625. BABBA/BABBA.py
@@ -0,0 +1,28 @@
+import sys
+import os
+
+# 운영 체제 확인
+if os.name == 'nt':  # Windows 환경
+    sys.stdin = open('input.txt', 'r')
+
+args = input()
+
+
+def fibonacci(n):
+    if n == 1:
+        print(0, 1)
+        return 
+    elif n == 2:
+        print(1, 1)
+        return
+    elif n == 3:
+        print(1, 2)
+        return
+    dp = [0, 1, 1, 2]
+    for i in range(4, n + 1):
+        dp.append(dp[i - 2] + dp[i - 1])
+
+    print(dp[n - 1], dp[n])
+
+
+fibonacci(int(args))
