13b3275f0e54660a7f47666ded9e4ffdf23d3e90
Jihun Jeong <demd7362@gmail.com>
2024-01-01 16:52:42 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.py
@@ -0,0 +1,11 @@
+import sys
+import os
+
+# 운영 체제 확인 및 입력 파일 설정 (필요한 경우)
+if os.name == 'nt':  # Windows 환경
+    sys.stdin = open('input.txt', 'r')
+
+n = int(input())
+
+for i in range(n):
+    print('*' * (i + 1))
