38a24c9c7dea6f7b237be7b3473c30b395a6f3ae
Jihun Jeong <demd7362@gmail.com>
2024-01-01 18:15:33 +0900

    [Bronze V] Title: 시험 성적, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,23 @@
+import sys
+import os
+
+# 운영 체제 확인 및 입력 파일 설정 (필요한 경우)
+if os.name == 'nt':  # Windows 환경
+    sys.stdin = open('input.txt', 'r')
+
+n = int(input())
+
+
+def get_rank(score):
+    if score >= 90:
+        return 'A'
+    elif score >= 80:
+        return 'B'
+    elif score >= 70:
+        return 'C'
+    elif score >= 60:
+        return 'D'
+    else:
+        return 'F'
+
+print(get_rank(n))
