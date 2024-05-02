5d5099cf9be2b1b107a974eeb8f568558d97013b
Jihun Jeong <demd7362@gmail.com>
2024-05-02 22:33:08 +0900

    [Gold IV] Title: 숨바꼭질 4, Time: 532 ms, Memory: 23128 KB -BaekjoonHub

--- 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.js
+++ 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.js
@@ -1,55 +1,51 @@
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-
-let input = fs.readFileSync(filePath)
-    .toString()
-    .trim()
-    // .split('\n')
-    // .map(x => x.trim());
+let input = fs.readFileSync(filePath).toString().trim();
+// .split('\n')
+// .map(x => x.trim());
 
 function solution(nk) {
-    const [n, k] = nk.split(' ').map(Number);
-    const visited = Array(100001).fill(false);
-    const prev = Array(100001).fill(null);
-    const functions = [
-        (x) => x + 1,
-        (x) => x - 1,
-        (x) => x * 2
-    ]
-
-    const bfs = (startValue) => {
-        const queue = [startValue];
-        visited[startValue] = true;
-
-        while (queue.length > 0) {
-            const value = queue.shift();
-
-            if (value === k) {
-                const path = [];
-                let current = value;
-                while (current !== null) {
-                    path.unshift(current);
-                    current = prev[current];
-                }
-                console.log(path.length - 1);
-                console.log(path.join(' '));
-                break;
-            }
-
-            for (const func of functions) {
-                const newValue = func(value);
-                if (newValue < 0 || newValue > 100000 || visited[newValue]) {
-                    continue;
-                }
-                queue.push(newValue);
-                visited[newValue] = true;
-                prev[newValue] = value;
-            }
+  const [n, k] = nk.split(' ').map(Number);
+  const visited = Array(100001).fill(false);
+  const prev = Array(100001).fill(null);
+  const functions = [
+    (x) => x + 1,
+    (x) => x - 1,
+    (x) => x * 2
+  ];
+  
+  const bfs = (startValue) => {
+    const queue = [startValue];
+    visited[startValue] = true;
+    
+    while (queue.length > 0) {
+      const value = queue.shift();
+      
+      if (value === k) {
+        const path = [];
+        let current = value;
+        while (current !== null) {
+          path.push(current);
+          current = prev[current];
         }
+        console.log(path.length - 1);
+        console.log(path.reverse().join(' '));
+        break;
+      }
+      
+      for (const func of functions) {
+        const newValue = func(value);
+        if (newValue < 0 || newValue > 100000 || visited[newValue]) {
+          continue;
+        }
+        queue.push(newValue);
+        visited[newValue] = true;
+        prev[newValue] = value;
+      }
     }
-    bfs(n);
-
+  }
+  
+  bfs(n);
 }
-
 
 solution(input);
