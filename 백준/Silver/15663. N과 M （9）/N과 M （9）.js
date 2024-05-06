b39d66d59e82041c76030d23a96a12fd2a521452
Jihun Jeong <demd7362@gmail.com>
2024-05-06 18:04:07 +0900

    [Silver II] Title: N과 M (9), Time: 224 ms, Memory: 17244 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.js
@@ -0,0 +1,36 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath)
+    .toString()
+    .trim()
+    .split("\n")
+    .map(x => x.trim())
+
+
+function solution([nm, args]) {
+    const [n, m] = nm.split(' ').map(Number);
+    const visited = new Array(n).fill(false);
+    const sequence = new Array(m).fill(0);
+    const numbers = args.split(' ').map(Number).sort((a, b) => a - b);
+    let answer = [];
+    const dfs = (depth) => {
+        if (depth === m) {
+            answer.push(sequence.join(' '));
+            return;
+        }
+        for (let i = 0; i < numbers.length; i++) {
+            if(visited[i]){
+                continue;
+            }
+            sequence[depth] = numbers[i];
+            visited[i] = true;
+            dfs(depth + 1);
+            visited[i] = false;
+        }
+    }
+    dfs(0);
+    console.log([...new Set(answer)].join('\n'));
+}
+
+
+solution(input);
