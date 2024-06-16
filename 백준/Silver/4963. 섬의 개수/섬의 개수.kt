5adef04fb8e682b44ce15c27bec8c2a167f77b4c
Jihun Jeong <demd7362@gmail.com>
2024-06-16 20:27:05 +0900

    [Silver II] Title: 섬의 개수, Time: 272 ms, Memory: 28856 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.kt
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.kt
@@ -5,41 +5,36 @@ import java.util.*
 
 fun main() {
     val os = System.getProperty("os.name").lowercase(Locale.getDefault())
-    val args: List<String> = if (os == "linux") {
+    val args: List<String> = if (os.contains("linux")) {
         val br = BufferedReader(InputStreamReader(System.`in`))
         br.readLines()
     } else {
         val inputFile = File("src/main/kotlin/input.txt")
         inputFile.readLines()
     }
-    val directions = listOf(listOf(0, 1), listOf(1, 0), listOf(-1, 0), listOf(0, -1),
+
+    val directions = listOf(
+        listOf(0, 1), listOf(1, 0), listOf(-1, 0), listOf(0, -1),
         listOf(1, 1), listOf(1, -1), listOf(-1, 1), listOf(-1, -1)
-        )
-    fun bfs(board: List<List<Int>>, row: Int, col: Int, startX:Int, startY: Int, visited: Array<BooleanArray>): Boolean {
+    )
+
+    fun bfs(board: List<List<Int>>, row: Int, col: Int, startX: Int, startY: Int, visited: Array<BooleanArray>) {
         val queue: Queue<IntArray> = LinkedList()
         queue.add(intArrayOf(startX, startY))
-        var flag = false
         while (queue.isNotEmpty()) {
             val (x, y) = queue.poll()
             if (visited[x][y]) {
                 continue
             }
-            if(board[x][y] == 1){
-                flag = true
-            }
             visited[x][y] = true
             for ((dx, dy) in directions) {
                 val nx = dx + x
                 val ny = dy + y
-                if (nx < 0 || ny < 0 || nx >= row || ny >= col || board[nx][ny] == 0 || visited[nx][ny]) {
-                    continue
+                if (nx in 0 until row && ny in 0 until col && board[nx][ny] == 1 && !visited[nx][ny]) {
+                    queue.add(intArrayOf(nx, ny))
                 }
-                queue.add(intArrayOf(nx, ny))
             }
         }
-        return flag
-
-
     }
 
     var i = 0
@@ -60,18 +55,15 @@ fun main() {
             board.add(args[j].split(' ').map { it.toInt() })
         }
         var count = 0
-        for(x in 0 until board.size){
-            for(y in 0 until board[x].size){
-                if(board[x][y] == 1){
-                    val result = bfs(board, h, w, x,y, visited)
-                    count += if (result) 1 else 0
+        for (x in 0 until h) {
+            for (y in 0 until w) {
+                if (board[x][y] == 1 && !visited[x][y]) {
+                    bfs(board, h, w, x, y, visited)
+                    count++
                 }
-
             }
         }
         println(count)
-
         i += h + 1
     }
-
 }
