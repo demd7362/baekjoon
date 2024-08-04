39e784f93ed7b4da2382a88f656551b5763be2e1
정지훈 <demd7362@gmail.com>
2024-08-04 13:12:18 +0900

    [Silver I] Title: n단 논법, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15723. n단 논법/n단 논법.py
@@ -0,0 +1,29 @@
+import os
+import sys
+
+if os.name == 'nt':  # Windows 환경
+    sys.stdin = open('input.txt', 'r')
+
+n = int(input())
+
+dic = {}
+reverse_dic = {}
+for i in range(n):
+    first, _is, second = input().split()
+    dic[first] = second
+    reverse_dic[second] = first
+
+m = int(input())
+
+for i in range(m):
+    first, _is, second = input().split()
+
+    key = first
+    while dic.get(key) != second and key is not None:
+        key = dic.get(key)
+    if dic.get(key) == second:
+        print('T')
+    else:
+        print('F')
+
+
