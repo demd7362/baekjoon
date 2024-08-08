01032e1b18aa590add821b27f9b868817178b41c
정지훈 <demd7362@gmail.com>
2024-08-08 11:04:46 +0900

    [Silver I] Title: 피보나치, Time: 128 ms, Memory: 114476 KB -BaekjoonHub

---
+++ 백준/Silver/9009. 피보나치/피보나치.py
@@ -0,0 +1,38 @@
+import os
+import sys
+from collections import deque
+
+if os.name == 'nt':  # Windows 환경
+    sys.stdin = open('input.txt', 'r')
+
+
+def fibo(n):
+    if n == 0:
+        return [0]
+    if n == 1:
+        return [0, 1]
+    dp = [0] * (n + 1)
+    dp[0] = 0
+    dp[1] = 1
+    for i in range(2, n + 1):
+        dp[i] = dp[i - 2] + dp[i - 1]
+    return dp
+
+
+cnt = int(input())
+
+fibo_list = fibo(44)
+results = []
+for _ in range(cnt):
+    n = int(input())
+    q = deque()
+    while n:
+        q = deque()
+        while n > 0:
+            for i in range(len(fibo_list) - 1, -1, -1):
+                if fibo_list[i] <= n:
+                    q.appendleft(str(fibo_list[i]))
+                    n -= fibo_list[i]
+                    break
+        results.append(' '.join(q))
+print('\n'.join(results))
