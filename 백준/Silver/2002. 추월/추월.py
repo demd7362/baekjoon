a3e2792472976c9dd3928b30613c439d5f90f665
정지훈 <demd7362@gmail.com>
2024-09-26 14:28:30 +0900

    [Silver II] Title: 추월, Time: 88 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2002. 추월/추월.py
+++ 백준/Silver/2002. 추월/추월.py
@@ -6,27 +6,15 @@ if os.name == 'nt':  # Windows 환경
 
 n = int(input())
 enters = [input() for _ in range(n)]
-exits = [input() for _ in range(n)]
+enter_dict = {enter: i for i, enter in enumerate(enters)}
+exits = [enter_dict[input()] for _ in range(n)]  # index 나간 순서 value 들어온 순서
 
-enter_dict = {}
-for i, enter_car in enumerate(enters):
-    enter_dict[enter_car] = set([enters[j] for j in range(i)])
 answer = 0
-
-exit_dict = {}
-for i, exit_car in enumerate(exits):
-    front_set = enter_dict[exit_car]
-    if not front_set:
-        continue
-    for j in range(i):
-        front_car = exits[j]
-        if front_car in front_set:
-            front_set.remove(front_car)
-    if len(front_set) > 0:
-        answer += 1
-
-
+for i in range(n):
+    for j in range(i + 1, n):
+        # print(f'exits[{i}]({exits[i]}) > exits[{j}]({exits[j]}) {exits[i] > exits[j]}')
+        if exits[i] > exits[j]:
+            # print(f'{enters[i]}는 추월함')
+            answer += 1
+            break
 print(answer)
-
-
-
