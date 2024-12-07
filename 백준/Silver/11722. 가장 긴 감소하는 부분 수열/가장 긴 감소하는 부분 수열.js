91681ce5574d89ae16c1995145bfe71cf964b18a
Jihun Jeong <demd7362@gmail.com>
2024-12-07 09:20:01 +0900

    [Silver II] Title: 가장 긴 감소하는 부분 수열, Time: 164 ms, Memory: 9660 KB -BaekjoonHub

---
+++ 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt"
+const input = fs.readFileSync(filePath)
+  .toString()
+  .trim()
+  .split("\n")
+
+
+function solution([n, args]) {
+  n = +n
+  let digits = args.split(' ').map(Number)
+  let dp = Array(n).fill(0)
+  for(let i = 0; i < n; i++){
+    let count = 0
+    for(let j = 0 ; j < i; j++){
+      if(digits[i] < digits[j] && count < dp[j]){
+        count = dp[j]
+      }
+    }
+    dp[i] = count + 1
+  }
+  console.log(Math.max(...dp))
+  // console.log(dp)
+}
+
+solution(input)
