e32447e415786ae39ebc3c5a84662bc22f494190
Jihun Jeong <demd7362@gmail.com>
2025-01-07 09:59:48 +0900

    [Gold V] Title: 두 용액, Time: 204 ms, Memory: 26464 KB -BaekjoonHub

---
+++ 백준/Gold/2470. 두 용액/두 용액.js
@@ -0,0 +1,34 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt"
+const input = fs.readFileSync(filePath)
+  .toString()
+  .trim()
+  .split("\n")
+
+// .map(x => x.trim())
+
+
+function solution([n, arg]) {
+  n = +n
+  let digits = arg.split(' ').map(Number)
+  digits.sort((a, b) => a - b)
+  let left = 0
+  let right = n - 1
+  let min = Infinity
+  let ans = []
+  while (left < right) {
+    let sum = Math.abs(digits[left] + digits[right])
+    if(sum < min){
+      min = sum
+      ans = [digits[left], digits[right]]
+    }
+    if (digits[left] + digits[right] < 0) {
+      left += 1
+    } else {
+      right -= 1
+    }
+  }
+  console.log(ans.join(' '))
+}
+
+solution(input)
