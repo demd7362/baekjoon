d3d5240b70474b9b2f5a01e69a403255399a7b74
Jihun Jeong <demd7362@gmail.com>
2025-01-14 21:47:33 +0900

    [Silver I] Title: 레슬러, Time: 200 ms, Memory: 16208 KB -BaekjoonHub

--- 백준/Silver/1900. 레슬러/레슬러.js
+++ 백준/Silver/1900. 레슬러/레슬러.js
@@ -9,30 +9,15 @@ const input = fs.readFileSync(filePath)
 
 function solution([n, ...args]) {
   n = +n
-  let canWin = (a, b) => {
-    return a.power + (b.power * a.ring) > b.power + (a.power * b.ring)
+  let compare = (a, b) => {
+    return (a.power + (b.power * a.ring)) - (b.power + (a.power * b.ring))
   }
-  let info = args.map(arg => {
+  let players = args.map((arg, i) => {
     let [power, ring] = arg.split(' ').map(Number)
-    return {power, ring}
-  })
-  let golds = Array(n).fill(0)
-  for (let i = 0; i < n; i++) {
-    for (let j = i + 1; j < n; j++) {
-      let iWin = canWin(info[i], info[j])
-      if (iWin) {
-        golds[i]++
-      } else {
-        golds[j]++
-      }
-    }
-  }
-  golds = golds.map((arg, i) => ({
-    index: i + 1,
-    gold: arg
-  }))
-  golds = golds.sort((a, b) => b.gold - a.gold).map(arg => arg.index)
-  console.log(golds.join('\n'))
+    return {power, ring, index: i + 1}
+  }).sort((a, b) => compare(b, a))
+    .map(arg => arg.index)
+  console.log(players.join('\n'))
 }
 
 solution(input)
