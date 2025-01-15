0d0cdd6f205ec6abbe13268e4308bcc76e6a57fa
Jihun Jeong <demd7362@gmail.com>
2025-01-15 15:56:45 +0900

    [Silver I] Title: 숫자놀이, Time: 92 ms, Memory: 9628 KB -BaekjoonHub

--- 백준/Silver/1679. 숫자놀이/숫자놀이.js
+++ 백준/Silver/1679. 숫자놀이/숫자놀이.js
@@ -8,38 +8,28 @@ const input = fs.readFileSync(filePath)
 function solution([n, args, limit]) {
   const digits = args.split(' ').map(Number);
   limit = Number(limit);
+  const dp = Array(1001).fill(Infinity);
+  dp[0] = 0; 
 
-  const bfs = (target) => {
-    let q = [{current: 0, depth: 0}];
-    let visited = new Set([0]); // 이미 방문한 값들
-    let front = 0;
-    while (front < q.length) {
-      const {current, depth} = q[front++];
+  for (let digit of digits) {
+    for (let i = digit; i < dp.length; i++) {
+      dp[i] = Math.min(dp[i], dp[i - digit] + 1);
+    }
+  }
 
-      if (current === target) return true;
+  let i = 1;
 
-      if (depth >= limit) continue;
-
-      for (const digit of digits) {
-        const nextValue = current + digit;
-        if (!visited.has(nextValue)) {
-          visited.add(nextValue);
-          q.push({current: nextValue, depth: depth + 1});
-        }
+  // 게임 진행
+  while (i <= dp.length) {
+    if (dp[i] > limit) {
+      if (i % 2 === 0) {
+        return `holsoon win at ${i}`;
+      } else {
+        return `jjaksoon win at ${i}`;
       }
     }
-    return false;
-  };
-
-  let target = 1;
-  while (true) {
-    if (!bfs(target)) {
-      console.log(`${target % 2 === 0 ? 'holsoon' : 'jjaksoon'} win at ${target}`);
-      break;
-    }
-    target++;
+    i++;
   }
 }
 
-
-solution(input)
+console.log(solution(input))
